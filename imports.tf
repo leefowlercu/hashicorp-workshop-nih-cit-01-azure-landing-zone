b1d8ff8ca49bc89e351a266853857ed93f97f1ff
Lee Fowler <lee.fowler@hashicorp.com>
2025-08-28 17:43:49 -0400

    fix: revert importids.json and imports.tf to placeholder values

--- imports.tf
+++ imports.tf
@@ -1,59 +1,59 @@
 import {
-  to = azurerm_resource_group.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra"
+  to = module.landing_zone.azurerm_resource_group.main
+  id = "<resource_group_import_id>"
 }
 
 import {
-  to = azurerm_storage_account.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Storage/storageAccounts/stnihcitworkshopnonprod"
+  to = module.landing_zone.azurerm_storage_account.main
+  id = "<storage_account_import_id>"
 }
 
 import {
-  to = azurerm_virtual_network.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra"
+  to = module.landing_zone.azurerm_virtual_network.main
+  id = "<virtual_network_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["web_zone_1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-1"
+  to = module.landing_zone.azurerm_subnet.subnets["web-zone-1"]
+  id = "<subnet_web_zone_1_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["web_zone_2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-2"
+  to = module.landing_zone.azurerm_subnet.subnets["web-zone-2"]
+  id = "<subnet_web_zone_2_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["web_zone_3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-3"
+  to = module.landing_zone.azurerm_subnet.subnets["web-zone-3"]
+  id = "<subnet_web_zone_3_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["app_zone_1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-1"
+  to = module.landing_zone.azurerm_subnet.subnets["app-zone-1"]
+  id = "<subnet_app_zone_1_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["app_zone_2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-2"
+  to = module.landing_zone.azurerm_subnet.subnets["app-zone-2"]
+  id = "<subnet_app_zone_2_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["app_zone_3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-3"
+  to = module.landing_zone.azurerm_subnet.subnets["app-zone-3"]
+  id = "<subnet_app_zone_3_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["data_zone_1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-1"
+  to = module.landing_zone.azurerm_subnet.subnets["data-zone-1"]
+  id = "<subnet_data_zone_1_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["data_zone_2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-2"
+  to = module.landing_zone.azurerm_subnet.subnets["data-zone-2"]
+  id = "<subnet_data_zone_2_import_id>"
 }
 
 import {
-  to = azurerm_subnet.subnets["data_zone_3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-3"
+  to = module.landing_zone.azurerm_subnet.subnets["data-zone-3"]
+  id = "<subnet_data_zone_3_import_id>"
 }
