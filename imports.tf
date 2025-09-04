4bc8814a101a6146a58c8c04406931fd41af4141
Lee Fowler <lee.fowler@hashicorp.com>
2025-09-04 09:31:57 -0400

    fix: comment out import blocks for default module showcase behavior

--- imports.tf
+++ imports.tf
@@ -1,59 +1,59 @@
-import {
-  to = module.landing_zone.azurerm_resource_group.main
-  id = "<resource_group_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_resource_group.main
+#   id = "<resource_group_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_storage_account.main
-  id = "<storage_account_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_storage_account.main
+#   id = "<storage_account_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_virtual_network.main
-  id = "<virtual_network_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_virtual_network.main
+#   id = "<virtual_network_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-1"]
-  id = "<subnet_web_zone_1_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-1"]
+#   id = "<subnet_web_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-2"]
-  id = "<subnet_web_zone_2_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-2"]
+#   id = "<subnet_web_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-3"]
-  id = "<subnet_web_zone_3_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-3"]
+#   id = "<subnet_web_zone_3_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-1"]
-  id = "<subnet_app_zone_1_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-1"]
+#   id = "<subnet_app_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-2"]
-  id = "<subnet_app_zone_2_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-2"]
+#   id = "<subnet_app_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-3"]
-  id = "<subnet_app_zone_3_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-3"]
+#   id = "<subnet_app_zone_3_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-1"]
-  id = "<subnet_data_zone_1_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-1"]
+#   id = "<subnet_data_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-2"]
-  id = "<subnet_data_zone_2_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-2"]
+#   id = "<subnet_data_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-3"]
-  id = "<subnet_data_zone_3_import_id>"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-3"]
+#   id = "<subnet_data_zone_3_import_id>"
+# }
