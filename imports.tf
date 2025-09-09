2bbbeeb5bf07c6efd2ec8b448bedad824602a68f
Lee Fowler <lee.fowler@hashicorp.com>
2025-09-09 13:36:31 -0400

    Revert "feat: add imports"
    This reverts commit 675d4eaa335c1cafddcd60f3096310a59f2f096b.

--- imports.tf
+++ imports.tf
@@ -1,59 +1,59 @@
-import {
-  to = module.landing_zone.azurerm_resource_group.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra"
-}
+# import {
+#   to = module.landing_zone.azurerm_resource_group.main
+#   id = "<resource_group_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_storage_account.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Storage/storageAccounts/stnihcitworkshopnonprod"
-}
+# import {
+#   to = module.landing_zone.azurerm_storage_account.main
+#   id = "<storage_account_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_virtual_network.main
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra"
-}
+# import {
+#   to = module.landing_zone.azurerm_virtual_network.main
+#   id = "<virtual_network_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-1"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-1"]
+#   id = "<subnet_web_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-2"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-2"]
+#   id = "<subnet_web_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["web-zone-3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/web-zone-3"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["web-zone-3"]
+#   id = "<subnet_web_zone_3_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-1"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-1"]
+#   id = "<subnet_app_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-2"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-2"]
+#   id = "<subnet_app_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["app-zone-3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/app-zone-3"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["app-zone-3"]
+#   id = "<subnet_app_zone_3_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-1"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-1"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-1"]
+#   id = "<subnet_data_zone_1_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-2"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-2"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-2"]
+#   id = "<subnet_data_zone_2_import_id>"
+# }
 
-import {
-  to = module.landing_zone.azurerm_subnet.subnets["data-zone-3"]
-  id = "/subscriptions/863a90b0-8aca-4579-aac3-a76ffa0ebe78/resourceGroups/rg-nih-cit-workshop-nonprod-base-infra/providers/Microsoft.Network/virtualNetworks/vnet-nih-cit-workshop-nonprod-base-infra/subnets/data-zone-3"
-}
+# import {
+#   to = module.landing_zone.azurerm_subnet.subnets["data-zone-3"]
+#   id = "<subnet_data_zone_3_import_id>"
+# }
